b54e25c77a03bcac714b09c35ccaedcde9338039
mv-kan <michael.kan.mail@gmail.com>
2022-08-05 12:43:51 +0200

    room service

--- src/go.mod
+++ src/go.mod
@@ -5,6 +5,7 @@ go 1.18
 require gorm.io/driver/postgres v1.3.8
 
 require (
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -18,6 +19,10 @@ require (
 	github.com/lib/pq v1.10.6 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gorm.io/gorm v1.23.8 // indirect
 )
--- src/service/room_service.go
+++ src/service/room_service.go
@@ -1,1 +1,69 @@
 package service
+
+import (
+	"github.com/mv-kan/the-school-project/entity"
+	"github.com/mv-kan/the-school-project/repo"
+)
+
+type IRoomService interface {
+	IService[entity.Room]
+	FindRoomType(room_id int) (entity.RoomType, error)
+	// check available space in room
+	FindAvailableSpace(room_id int) (int, error)
+	// get all residents in a room
+	FindAllResidents(room_id int) ([]entity.Pupil, error)
+}
+
+type roomService struct {
+	service[entity.Room]
+	roomTypeRepo repo.IRepository[entity.RoomType]
+	pupilRepo    repo.IRepository[entity.Pupil]
+}
+
+func (s roomService) FindAvailableSpace(room_id int) (int, error) {
+	// check for existing
+	_, err := s.r.Find(room_id)
+	if err != nil {
+		return -1, err
+	}
+	roomType, err := s.FindRoomType(room_id)
+	if err != nil {
+		return -1, err
+	}
+	residents, err := s.FindAllResidents(room_id)
+	if err != nil {
+		return -1, err
+	}
+	return roomType.MaxOfResidents - len(residents), nil
+}
+
+func (s roomService) FindAllResidents(room_id int) ([]entity.Pupil, error) {
+	// check for exising
+	_, err := s.r.Find(room_id)
+	if err != nil {
+		return nil, err
+	}
+	pupils, err := s.pupilRepo.FindAll()
+	if err != nil {
+		return nil, err
+	}
+	residents := make([]entity.Pupil, 0)
+	for _, pupil := range pupils {
+		if pupil.RoomID.Valid && pupil.RoomID.Int32 == int32(room_id) {
+			residents = append(residents, pupil)
+		}
+	}
+	return residents, nil
+}
+
+func (s roomService) FindRoomType(room_id int) (entity.RoomType, error) {
+	room, err := s.r.Find(room_id)
+	if err != nil {
+		return entity.RoomType{}, err
+	}
+	roomType, err := s.roomTypeRepo.Find(room.RoomTypeID)
+	if err != nil {
+		return entity.RoomType{}, err
+	}
+	return roomType, nil
+}
